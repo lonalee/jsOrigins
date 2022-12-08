768a42349976ff499b5effcafee54c60d87d6780
lonalee2018 <lonalee2018@gmail.com>
2022-12-08 14:10:28 +0900

    파라미터들을 배열로 전달하기
    동적인 맵 키/밸류 쌍을 생성한다.

--- data/Map/index.js
+++ data/Map/index.js
@@ -31,7 +31,7 @@ const clearAllFilters = (key, value) => {
 addFilter('나이', 2);
 addFilter('견종', '핏불');
 delFilter('견종');
-console.log('filters 확인 --------->',filters)
+// console.log('filters 확인 --------->',filters)
 // ------------------------------------------------------------
 
 const collection2 = [{ data: '1' }, { data: '2' }, 3];
@@ -45,11 +45,40 @@ const someObj = {
   견종: '요크셔',
   나이: 10,
 };
-let filters2 = new Map([
-  [1, someObj],
-  [2, { ...someObj, 나이: 11 }],
-]);
+// 필터를 생성한다
+const makeMapFilters = (key, value) => {
+  let filters2 = new Map([
+    [key,value]
+    // [key2,value2] 
+    // [key3,value3] ...
+  ])
+  return filters2
+}
+const makeMapFilters2 = (keys, values) => {
+  let filters2 = new Map([])
+  keys.forEach((key,i) => {
+    filters2.set(key, values[i])
+  })
 
-console.log('필터2-->', filters2);
-const filters2keys = filters2.keys();
-console.log('필터2 키값만 이터레이터-->', filters2keys)
+  return filters2
+}
+
+// 파라미터들을 배열로 전달하기
+const mapFilter = makeMapFilters2(['견종','나이'], ['리트리버',9])
+// const mapFilter2 = makeMapFilters2({
+//   '견종':'요크셔', '나이':8
+// })
+
+console.log('필터2-->', mapFilter);
+
+
+console.log('필터2-->', mapFilter.keys());
+
+// let filters2 = new Map([
+//   [1, someObj],
+//   [2, { ...someObj, 나이: 11 }],
+// ]);
+
+// console.log('필터2-->', filters2);
+// const filters2keys = filters2.keys();
+// console.log('필터2 키값만 이터레이터-->', filters2keys)
