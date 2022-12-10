de14a9af522cb6fbb8734cffb9a18d2e1ab8a584
unknown <lonalee2018@gmail.com>
2022-12-11 01:03:39 +0900

    코드 정리

--- data/Map/index.js
+++ data/Map/index.js
@@ -7,44 +7,10 @@
  * 2. 기준 객체로 배열을 필터링 (find/filter 메소드 사용)
  * cons : filters 객체(순수한 오브젝트)를 다루기
  */
-const handleObject = () => {
-  const collection = [
-    { name: 'paul', 견종: '요크셔', 나이: 1 },
-    { name: 'terry', 견종: '요크셔', 나이: 9 },
-    { name: 'charm', 견종: '요크셔', 나이: 4 },
-    { name: 'edge', 견종: '요크셔', 나이: 2 },
-  ];
-
-  let filters = {};
-
-  // --- cons ---
-  const addFilter = (key, value) => {
-    filters[key] = value;
-  };
-  const delFilter = (key) => {
-    delete filters[key];
-  };
-  const clearAllFilters = (key, value) => {
-    filters = {};
-  };
-  // --- cons ---
-  addFilter('나이', 2);
-  addFilter('견종', '핏불');
-  delFilter('견종');
-  // console.log('filters 확인 --------->',filters)
-  // ------------------------------------------------------------
-};
 
 const handleMap = () => {
   // 필터를 생성하는 함수 작성해보기
-  const makeMapFilters = (key, value) => {
-    let filters2 = new Map([
-      [key, value],
-      // [key2,value2]
-      // [key3,value3] ...
-    ]);
-    return filters2;
-  };
+  
   const makeMapFilters2 = (keys, values) => {
     let filters2 = new Map([]);
     keys.forEach((key, i) => {
@@ -54,33 +20,17 @@ const handleMap = () => {
   };
 
   // 파라미터(키/밸류 페어)들을 배열로 전달하기
+  // 더 좋은 방법이 있을지 모른다.
   const mapFilter = makeMapFilters2(
     ['색상', '견종', '나이'],
     ['옐로', '리트리버', 9]
   );
-  // const mapFilter2 = makeMapFilters2({
-  //   '견종':'요크셔', '나이':8
-  // })
-
-  // for (const iterator of mapFilter) {
-  // console.log('chk iterator -->', iterator);
-  // }
-
-  // forin은 동작 안함
-  for (const key in mapFilter) {
-    console.log('&&&&&&&&&&&&&&&&&&');
-  }
 
   // mapFilter.keys()
   // mapFilter.entries() // 키/밸류 쌍을 하나의 배열에 각각 담아 맵 엔트리 형태로 리턴한다.
   //맵 엔트리(이터레이터?)는 또 뭔가?
 
   const spread = [...mapFilter];
-  // console.log('spread syntax', spread);
-  // console.log('spread syntax', spread.sort()); // 정렬 가능!!
-  // console.log('spread syntax', spread[1]);
-  // console.log('원본 비교 ', mapFilter);
-
   /**
    * mapFilter =>
    * {
@@ -95,6 +45,7 @@ const handleMap = () => {
 
   for (const [key, value] of spreadMapFilter.sort()) {
     // console.log("chk key,value -->", key, value);
+    // 그냥 배열로 만들어서 작업하자
   }
 
   const arr = spreadMapFilter.sort().map((iter) => {
